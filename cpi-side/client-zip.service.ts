bbc65c7c53b1375bacd6fc8c61916db3d22a4345
TomerPopik <tomer.p@pepperi.com>
2023-11-20 17:36:55 +0200

    - Added try catch

--- cpi-side/client-zip.service.ts
+++ cpi-side/client-zip.service.ts
@@ -17,29 +17,33 @@ class ClientZipService {
 
     // Create the symlink files in the client zip folder.
     async createSymlinksFiles(relativePath: string): Promise<void> {
-        const baseUrl = await pepperi.files.baseURL(); // file://localhost:8088/files
+        const baseUrl = await pepperi.files.rootDir();//.baseURL(); // file://localhost:8088/files
 
-        // Read file from path and read the data into fileData variable.
-        const fileData = await readFile(`${baseUrl}${relativePath}/addon_package.json`, 'utf8');
-        const clientZipData: IClientZipFilesData = JSON.parse(fileData);
-        
-        if (clientZipData && clientZipData.Symlinks) {
-            for (let index = 0; index < clientZipData.Symlinks.length; index++) {
-                const symlinkData: ISymlinkFilesData = clientZipData.Symlinks[index];
+        try {
+            // Read file from path and read the data into fileData variable.
+            const fileData = await readFile(`${baseUrl}${relativePath}/addon_package.json`, 'utf8');
+            const clientZipData: IClientZipFilesData = JSON.parse(fileData);
+            
+            if (clientZipData && clientZipData.Symlinks) {
+                for (let index = 0; index < clientZipData.Symlinks.length; index++) {
+                    const symlinkData: ISymlinkFilesData = clientZipData.Symlinks[index];
 
-                if (symlinkData.ExcludedFileName && symlinkData.ExcludedFileName.length > 0) {
-                    const symlinkName = symlinkData.ExcludedFileName;
-                    const symlinkTargetPath = `${baseUrl}/Addon/Public/${AddonUUID}/${this.addonVersion}/assets/externals${symlinkData.AdditionalData.RelativePathToFolder}/${symlinkName}`; // symlink.RelativePathToOriginal.replace(AddonVersionString, AddonVersion);
-                    const symlinkPath = `${baseUrl}${relativePath}/${symlinkName}`;
+                    if (symlinkData.ExcludedFileName && symlinkData.ExcludedFileName.length > 0) {
+                        const symlinkName = symlinkData.ExcludedFileName;
+                        const symlinkTargetPath = `${baseUrl}/Addon/Public/${AddonUUID}/${this.addonVersion}/assets/externals${symlinkData.AdditionalData.RelativePathToFolder}/${symlinkName}`; // symlink.RelativePathToOriginal.replace(AddonVersionString, AddonVersion);
+                        const symlinkPath = `${baseUrl}${relativePath}/${symlinkName}`;
 
-                    try {
-                        await symlink(symlinkTargetPath, symlinkPath);
-                    } catch (err) {
-                        console.error(`Symlink creation failed: ${ err}`);
-                        throw new Error(`Symlink creation failed: ${ err}`);
+                        try {
+                            await symlink(symlinkTargetPath, symlinkPath);
+                        } catch (err) {
+                            console.error(`Symlink creation failed: ${err}`);
+                            // throw new Error(`Symlink creation failed: ${ err}`);
+                        }
                     }
                 }
             }
+        } catch (error) {
+            console.error(`Error read addon_package.json file: ${error}`);
         }
     }
 }
